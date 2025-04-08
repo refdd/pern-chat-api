a2b12b0e0e7a614a2f36b54a90c1bb9a29c46931
refdd <titorefat76@gmail.com>
2025-04-08 12:09:17 +0200

    implement messaging feature with socket.io and JWT authentication

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from "express";
-const app = express();
+import { app, server } from './socket/socket.js'
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+dotenv.config()
+app.use(express.json()) // for parsing application json
+app.use(cookieParser()) // for parsing cookies
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.json({ message: "api is working" });
 });
 
 const PORT = process.env.PORT || 3000;
