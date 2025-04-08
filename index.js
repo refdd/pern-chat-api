b5513daf93f6d053f138ca25a52618ded2ab9f3d
refdd <titorefat76@gmail.com>
2025-04-08 10:49:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+export default app;
